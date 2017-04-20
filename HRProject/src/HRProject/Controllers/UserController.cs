553aa4d32ad894c828e29dc689981df96b0a8f73
DOIT_DOTNET_USER8 <DOIT_DOTNET_USER8@C08>
2017-04-20 21:01:43 +0200

    Added UpdateUser

--- HRProject/src/HRProject/Controllers/UserController.cs
+++ HRProject/src/HRProject/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using HRProject.Services;
+﻿using HRProject.Models;
+using HRProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -42,9 +43,37 @@ namespace HRProject.Controllers
             _userRepository.GetUser(userId);
 
             return Ok(user);    
-
         }
 
 
+        [HttpPut("{userId}")]
+        public IActionResult UpdateUser(int userId, [FromBody] User updateUser)
+        {
+            if (updateUser == null || updateUser.UserId != userId)
+            {
+                return BadRequest();
+            }
+
+            var newUser = _userRepository.GetUser(userId);
+            if (newUser == null)
+            {
+                return NotFound();
+            }
+
+            newUser.Name = updateUser.Name;
+            newUser.SurName = updateUser.SurName;
+            newUser.City = updateUser.City;
+            newUser.Country = updateUser.Country;
+            newUser.PartTime_FullTime = updateUser.PartTime_FullTime;
+            newUser.WorkExperience = updateUser.WorkExperience;
+            newUser.Status = updateUser.Status;
+            newUser.DateOfBirth = updateUser.DateOfBirth;
+            newUser.Sex = updateUser.Sex;
+            newUser.NoteField = updateUser.NoteField;
+            newUser.Keywords = updateUser.Keywords;
+
+            _userRepository.UpdateUser(newUser);
+            return new NoContentResult();
+        }
     }
 }
