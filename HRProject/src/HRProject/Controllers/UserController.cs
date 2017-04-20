265cfe835c1617f7afe1c5d4683385bb0e2930fe
DOIT_DOTNET_USER8 <DOIT_DOTNET_USER8@C08>
2017-04-20 20:41:46 +0200

    HttpGet user

--- HRProject/src/HRProject/Controllers/UserController.cs
+++ HRProject/src/HRProject/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRProject.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +7,44 @@ using System.Threading.Tasks;
 
 namespace HRProject.Controllers
 {
-
+    [Route("api/user")]
     public class UserController : Controller
     {
         private HRContext _ctx;
+        private IUserRepository _userRepository;
+
         public UserController(HRContext ctx)
         {
             _ctx = ctx;
+            _userRepository = new UserRepository(ctx);
         }
+
+
+        [HttpGet()]
+        public IActionResult GetUsers()
+        {
+            var userEntity = _userRepository.GetUsers();
+
+            return Ok(userEntity);
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetUser(int userId)
+        {
+            var user = _userRepository.GetUser(userId);
+
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userRepository.GetUser(userId);
+
+            return Ok(user);    
+
+        }
+
+
     }
 }
