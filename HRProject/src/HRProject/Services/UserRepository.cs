a4a049d9e59e11b5969a1bf24de0ddf8d661b421
jela <jela.pezos@gmail.com>
2017-04-20 19:36:45 +0200

    User

---
+++ HRProject/src/HRProject/Services/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRProject.Models;
+
+namespace HRProject.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
