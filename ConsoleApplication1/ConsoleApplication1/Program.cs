df1992c40bba29276494d0caed3351167506718c
Stefan <Stefan@MYPC>
2017-05-16 23:22:40 +0200

    dasefdf

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan span = new TimeSpan(-7, -7, -7);
+            TimeSpan duration = span.Duration();
+            Console.WriteLine(duration);
+        }
+}
